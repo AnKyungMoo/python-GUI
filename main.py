4d48f0d78dd4ff4026fb71ff1e5d3dad2508767c
KM <sodp2@naver.com>
2018-11-28 10:00:28 +0900

    feat: add the label frame

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ win = tk.Tk()
 # 타이틀 설정
 win.title("과자 자판기")
 
+# window instance 대신에 사용할 mighfy Frame 생성
+mighfy = ttk.LabelFrame(win, text='자판기')
+mighfy.grid(column=0, row=0)
+
 ## 과자의 종류를 담는 배열
 snacks = ['새우', '감자', '오징어', '버터', '딸기', '오렌지']
 
@@ -17,11 +21,11 @@ def purchase():
     messageLabel.configure(text=snacks[index] + ' 과자를 샀다!')
     
 # 무엇을 구매했는지를 알려주기 위한 Label
-messageLabel = ttk.Label(win, text="Click the Purchase!")
+messageLabel = ttk.Label(mighfy, text="Click the Purchase!")
 messageLabel.grid(column=0, row=3)
 
 # 구매 버튼 생성
-purchaseButton = ttk.Button(win, text="Purchase!", command=purchase)
+purchaseButton = ttk.Button(mighfy, text="Purchase!", command=purchase)
 purchaseButton.grid(column=3, row=3)
 
 # 라디오 버튼에 바인딩할 정수타입의 변수
@@ -36,8 +40,14 @@ for ro in range(2):
         index = ro + (col + (ro * 2))
 
         # 라디오 버튼 생성
-        currentRadioButton = tk.Radiobutton(win, text=snacks[index], variable=radioVar, value=index)
+        currentRadioButton = tk.Radiobutton(mighfy, text=snacks[index], variable=radioVar, value=index)
         currentRadioButton.grid(column=col, row=ro, sticky=tk.W)
+
+# for loop를 이용하여 컴포넌트들을 모두 정렬
+for child in mighfy.winfo_children():
+    child.grid_configure(sticky='E')
+    
 
 # 메인 루프 시작
 win.mainloop()
+
