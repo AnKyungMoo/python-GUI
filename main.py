070d18e2ba6055bfad9048965cd0adb36dd282cd
KM <sodp2@naver.com>
2018-12-01 16:28:57 +0900

    feat: add the radio button tooltip

--- main.py
+++ main.py
@@ -3,6 +3,46 @@ from tkinter import ttk
 from tkinter import Menu
 from tkinter import messagebox as msg
 
+class ToolTip(object):
+    def __init__(self, widget):
+        self.widget = widget
+        self.tip_window = None
+
+    def show_tip(self, tip_text):
+        "Display text in a tooltip window"
+        if self.tip_window or not tip_text:
+            return
+
+        x, y, _cx, cy = self.widget.bbox("insert")
+        x = x + self.widget.winfo_rootx() + 25
+        y = y + cy + self.widget.winfo_rooty() + 25
+        self.tip_window = tw = tk.Toplevel(self.widget)
+        tw.wm_overrideredirect(True)
+
+        tw.wm_geometry("+%d+%d" % (x, y))
+
+        label = tk.Label(tw, text=tip_text, justify=tk.LEFT,
+                    background="#ffffe0", relief=tk.SOLID, borderwidth=1,
+                    font=("tohoma", "8", "normal"))
+        label.pack(ipadx=1)
+
+    def hide_tip(self):
+        tw = self.tip_window
+        self.tip_window = None
+        if tw:
+            tw.destroy()
+
+def create_ToolTip(widget, text):
+    toolTip = ToolTip(widget)
+
+    def enter(event):
+        toolTip.show_tip(text)
+    def leave(event):
+        toolTip.hide_tip()
+
+    widget.bind('<Enter>', enter)
+    widget.bind('<Leave>', leave)
+
 # window instance 생성
 win = tk.Tk()
 
@@ -77,6 +117,8 @@ for ro in range(2):
         # 라디오 버튼 생성
         currentRadioButton = tk.Radiobutton(mighfy, text=snacks[index], variable=radioVar, value=index)
         currentRadioButton.grid(column=col, row=ro, sticky=tk.W)
+        
+        create_ToolTip(currentRadioButton, snacks[index] + '과자입니다.')
 
 # for loop를 이용하여 컴포넌트들을 모두 정렬
 for child in mighfy.winfo_children():
