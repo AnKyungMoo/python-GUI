07c1b59ed2257f2846d0a76e49285f392980994b
KM <sodp2@naver.com>
2018-12-03 04:56:19 +0900

    feat: add the refill ui

--- Admin.py
+++ Admin.py
@@ -10,6 +10,11 @@ class Admin:
         # 타이틀 설정
         self.win.title("admin")
 
+        # 변수
+        self.snacks = ['Shrimp', 'Potato', 'Squid', 'Butter', 'Strawberry', 'Orange']
+        self.check_button_var = []
+
+        # 위젯 생성
         self.create_widgets()
 
     def create_widgets(self):
@@ -26,6 +31,17 @@ class Admin:
         scrolled_text = scrolledtext.ScrolledText(console_tab, width=80, height=30, wrap=tk.WORD)
         scrolled_text.grid(column=0, row=0)
 
+        # 체크박스 생성
+        for ro in range(2):
+            for col in range(3):
+                index = ro + (col + (ro * 2))
+                self.check_button_var.append(tk.IntVar())
+
+                check_button = tk.Checkbutton(refill_tab, text=self.snacks[index], variable=self.check_button_var[index])
+                check_button.grid(column=col, row=ro, sticky=tk.W)
+
+        refill_button = ttk.Button(refill_tab, text='refill!')
+        refill_button.grid(column=2, row=2)
 
 admin = Admin()
 admin.win.mainloop()
